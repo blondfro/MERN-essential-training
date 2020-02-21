5abe8a9eaede74e235aeb5d4c85e355ab721b57a
James Morris <blondfro@gmail.com>
2020-02-21 13:32:48 -0600

    installed axios to be able to get data from the backend and made the app component a class component.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,32 +1,67 @@
 import React from 'react';
+import axios from "axios";
 import './App.css';
 import PlayerList from "./player/PlayerList";
 import PlayerSingle from "./player/PlayerSingle";
 import PlayerForm from "./player/PlayerForm";
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col s12">
-          Menu
-        </div>
-      </div>
-      <div className="row">
-        <div className="col s3">
-          <PlayerList/>
-        </div>
-        <div className="col s9">
-          <PlayerSingle/>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col s12">
-          <PlayerForm/>
-        </div>
-      </div>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: [],
+            currentPlayer: {}
+        };
+
+        this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+    }
+
+    componentDidMount() {
+        const url = "http://localhost:4000/players";
+        axios.get(url)
+            .then((Response) => {
+            this.setState({player: Response.data})
+        })
+            .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    updateCurrentPlayer(item) {
+        this.setState({
+            currentPlayer: item,
+        })
+    }
+
+  render() {
+      return (
+          <div className="container-fluid">
+              <div className="row">
+                  <div className="col s12">
+                      Menu
+                  </div>
+              </div>
+              <div className="row">
+                  <div className="col s3">
+                      <PlayerList
+                          players = {this.state.players}
+                          updateCurrentPlayer = {this.updateCurrentPlayer}
+                      />
+                  </div>
+                  <div className="col s9">
+                      <PlayerSingle/>
+                  </div>
+              </div>
+              <div className="row">
+                  <div className="col s12">
+                      <PlayerForm/>
+                  </div>
+              </div>
+          </div>
+      );
+  }
+
+
 }
 
 export default App;
