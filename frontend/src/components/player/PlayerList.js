5f3e4d603a9e5eb71f763b8376f4f8c4590dc94a
James Morris <blondfro@gmail.com>
2020-02-21 14:03:50 -0600

    updated the list to display using materialize css and put the info into an a tag

--- frontend/src/components/player/PlayerList.js
+++ frontend/src/components/player/PlayerList.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-function PlayerList({players, ...props}) {
+function PlayerList({players, updateCurrentPlayer, ...props}) {
     return (
         <div>
-            {players.map( player => {
-                return <div key={player.firstName}>{player.firstName}</div>
-            })}
+            <ul className="collection with-header">
+                <li className="collection-header">
+                    <h4>Players</h4>
+                </li>
+                {players.map( player => {
+                    return <a
+                        href="#!"
+                        className="collection-item"
+                        key={player._id}
+                        onClick={updateCurrentPlayer.bind(this, player)}>
+                        {player.firstName} {player.lastName}
+                    </a>
+                })}
+            </ul>
+
+
         </div>
     );
 }
