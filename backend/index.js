abe5615f1723f1b1876f14c87f01d183620c4ca6
James Morris <blondfro@gmail.com>
2020-02-20 19:49:45 -0600

    used mongoose to connect to the local host mongo db.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyparser from "body-parser";
+
 
 const app = express();
 const PORT = 3000;
+
+// mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/soccerDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// body-parser setup for both urlencoded and json.
+app.use(bodyparser.urlencoded({ extended: true}));
+app.use(bodyparser.json());
 
 // get command
 app.get("/", (req, res) => {
